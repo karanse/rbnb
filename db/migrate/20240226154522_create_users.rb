a652e41e85b4bf4ef4965cfb220904423a725ee7
sema karan <karan18sema@gmail.com>
2024-02-26 16:52:05 +0100

    fix migration files

--- db/migrate/20240226154522_create_users.rb
+++ db/migrate/20240226154522_create_users.rb
@@ -3,7 +3,6 @@ class CreateUsers < ActiveRecord::Migration[7.1]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.string :email
       t.string :owner
 
       t.timestamps
