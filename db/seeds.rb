f65c5a1e39eea8d6839dacff754c9a24d518b558
sema karan <karan18sema@gmail.com>
2024-02-27 12:07:25 +0100

    add fake data for listings

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,23 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+puts "Cleaning the listings data..."
+Listing.destroy_all
+
+puts "Creating sample listing data..."
+#  Columns address, category, rating, price_per_night, has_pool, has_garden, number_of_rooms
+
+10.times do
+  listing = Listing.create!(address: Faker::Address.street_address,
+                            category: ["house", "apartment", "guesthouse", "cottage", "cabin", "yurt"].sample,
+                            rating: rand(0..5),
+                            price_per_night: rand(20..200),
+                            has_pool: ["true", "false"].sample,
+                            has_garden: ["true", "false"].sample,
+                            number_of_rooms: rand(1..5))
+  listing.save
+end
+
+puts "listings sample data created!"
