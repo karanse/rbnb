4f0737874a105b6fa1ee108940bb71a5cc41c3c6
Loes van Puijenbroek <loesvanpuijenbroek@gmail.com>
2024-03-01 14:33:53 +0100

    slight change heading + grid

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2024_03_01_111411) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
